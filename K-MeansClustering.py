831b2ad8d38a1b72a44fd927da300f433332c7ec
Aravind K S <ksaravind9@gmail.com>
2023-12-29 11:33:14 +0530

    Using K-Means clustering algorithm to cluster a set of data stored in a .CSV file.

---
+++ K-MeansClustering.py
@@ -0,0 +1,27 @@
+﻿import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+df = pd.read_csv('https://gist.githubusercontent.com/net/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv') 
+df
+df['variety'].unique()
+
+
+real_target = df.pop('variety') 
+real_target.replace({ 'Setosa':0,
+                     'Virginica':2,
+                     'Versicolor':1}, inplace=True)
+
+
+model KMeans (n_clusters=3) 
+target_predict = model.fit_predict(df)
+
+
+real target = np.array(real_target)
+
+
+target_predict
+
+
+real_target
